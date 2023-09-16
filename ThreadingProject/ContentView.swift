3bbf64f231724a991cc8476fe425061408e5a88f
Dilaneoz <dilansuozturk@gmail.com>
2023-09-16 17:02:34 +0300

    SwiftUIAsyncImage

---
+++ ThreadingProject/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ThreadingProject
+//
+//  Created by Atil Samancioglu on 17.08.2021.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            VStack {
+            // AsyncImage internetten verdiğimiz urlyi asenkron olarak indirir
+                AsyncImage(url: URL(string: "https://atilsamancioglu.com/wp-content/uploads/2018/01/photo2.jpg")!) { image in
+                    image.resizable().frame(width: 300, height: 275, alignment: .center)
+                } placeholder: { // placeholder göster indirilene kadar ne gösterileceği
+                    ProgressView() // ProgressView i göster
+                }
+                 
+                
+                
+                List(superHeroArray) { superhero in
+                    Text(superhero.name)
+                        .font(.title3)
+                        .foregroundColor(.blue)
+                }
+            }.navigationTitle(Text("Superhero Book"))
+                
+                     
+        }
+
+        }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
